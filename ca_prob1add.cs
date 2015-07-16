e7df0204e4b471e075bf16c570cb2ea4104bb1ae
Andy Y <andyyao3@gmail.com>
2015-07-16 11:19:02 -0700

    changed users info and added prob 1

---
+++ ca_prob1add.cs
@@ -0,0 +1,12 @@
+//Code abbey problem 1 Jul 16 2015
+using System;
+
+class Program{
+    public static void Main(){
+        int a, b, c;
+        a = 9224834;
+        b = 4889727;
+        c = a + b;
+        Console.WriteLine(c);
+    }
+}
