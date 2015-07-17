02ce48db23e007ee13ba4d59961a2fc42831f689
Andy Y <andyyao3@gmail.com>
2015-07-17 14:31:39 -0700

    figured out how solutions are related to .cs files solved prob #2

---
+++ ca_prob2loopsum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void CA2(string[] args)
+        {
+            string line;
+            string path = @"C:\\Users\andy.yao\\Desktop\\CA.txt";
+            System.IO.StreamReader file = new System.IO.StreamReader(path);
+            line = file.ReadLine().Skip(1).Take(1).First();
+            //
+            //while ((line = file.ReadLine()) != null)
+            //{
+            //    Console.WriteLine(line);
+            //}
+            Console.WriteLine(line);
+            file.Close();
+        }
+    }
+}
